2486e6016b6c3ea629af3449d40c81b230c7f98b
macdressler <macsedressler@gmail.com>
2024-02-07 09:52:59 -0700

    Found the widest line, now finding that centre and trying to test with real images from the drone. Need to discuss my drone at meeting

--- middleline.py
+++ middleline.py
@@ -12,7 +12,7 @@ import numpy as np
 
 """I need to make this into a function so that I can call it from the drone. Eventually, I need to make it so that it can see the centre of the main line.""" 
 img = cv2.imread('testimage.png')
-
+points = []
 
 def line_detection(img):
     # Reading the required image in
@@ -29,9 +29,11 @@ def line_detection(img):
     # This returns an array of r and theta values
     lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
     
+    
     # The below for loop runs till r and theta values
     # are in the range of the 2d array
     for r_theta in lines:
+        
         arr = np.array(r_theta[0], dtype=np.float64)
         r, theta = arr
         # Stores the value of cos(theta) in a
@@ -57,15 +59,46 @@ def line_detection(img):
     
         # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
         y2 = int(y0 - 1000*(a))
-    
+        points.extend([x1, y1, x2, y2])
         # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
         # (0,0,255) denotes the colour of the line to be
         # drawn. In this case, it is red.
         cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+     
     
     # All the changes made in the input image are finally
     # written on a new image houghlines.jpg
     cv2.imwrite('linesDetected.jpg', img)
     return print("success")
+"""Now I need to find the largest line"""
 
+def largest_line(points):
+    """The strategy is to determine if each line is horizontal or vertical"""
+    d = len(points)
+    print(d)
+    e = d/8
+    print(e)
+    print(int(e))
+    f = int(e)
+    width = []
+    for i in f:
+        """1 is horizontal, 2 is vertical. I am mostly looking for vertical lines, but I need the direction"""
+        direction = 1
+        if points[i * 8] == -1000:
+            direction = 1
+
+        else:
+            direction = 2
+        
+        if direction == 1:
+            width.extend(abs(points[(i * 8) + 1] - points[(i*8)+5]))
+        else:
+            width.extend(abs(points[(i * 8)] - points[(i*8)+4]))
+
+
+    return width
+        
 line_detection(img)
+print(points)
+width = largest_line(points)
+print(width)
